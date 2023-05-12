c5e7b0e0733fb767591b5829cbc1f092f2f9319a
leticiamfrota <128155229+leticiamfrota@users.noreply.github.com>
2023-05-12 14:29:54 +0000

    Exercício Concluído

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 """Main functions"""
 
+import string as stri
 
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
-    return False
+    string = string.replace(' ', '')
+    string = string.lower()
+    for simbolo in stri.punctuation:
+        string = string.replace(simbolo, '')
+    for indice, _caractere in enumerate(string):
+        if string[indice] != string[-indice-1]:
+            return False
+
+    return True
